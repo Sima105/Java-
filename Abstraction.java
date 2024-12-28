3e4af488c2f5411e4c5eda1e51040711d13248c2
Sima105 <simashah11112222@gmail.com>
2024-12-28 07:10:53 +0545

    Abstraction on java
    Previewing 2nd feature of OOP i.e. abstraction

---
+++ Abstraction.java
@@ -0,0 +1,28 @@
+abstract class Animal {
+    abstract void sound(){
+    void sleep() {
+        System.out.println("Sleeping...");
+    }
+}
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Dog(); 
+        Animal cat = new Cat(); 
+
+        dog.sound();
+        dog.sleep();
+
+        cat.sound();
+        cat.sleep();
+    }
+}
