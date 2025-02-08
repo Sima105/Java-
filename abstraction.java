254219eaf5b7a7be10df686319f8e29a37329591
Sima105 <simashah11112222@gmail.com>
2025-02-08 20:24:49 +0545

    Abstraction in java
    Practicing abstraction in java

---
+++ abstraction.java
@@ -0,0 +1,30 @@
+abstract class Vehicle {
+    abstract void start();
+    void stop() {
+        System.out.println("Vehicle is stopping...");
+    }
+}
+
+class Car extends Vehicle {
+    void start() {
+        System.out.println("Car is starting with a key...");
+    }
+}
+
+class Bike extends Vehicle {
+    void start() {
+        System.out.println("Bike is starting with a self-start button...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle myCar = new Car();
+        myCar.start(); 
+        myCar.stop();  
+
+        Vehicle myBike = new Bike();
+        myBike.start(); 
+        myBike.stop();  
+    }
+}
