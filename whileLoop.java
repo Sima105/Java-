4f6f0f1ce7892c1f14eca96f83044ef256f199b2
Sima105 <simashah11112222@gmail.com>
2025-03-08 11:41:01 +0545

    practicing while loop.

--- whileLoop.java
+++ whileLoop.java
@@ -1,1 +1,12 @@
 //performing while loop.
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1; // Initialization
+        
+        while (i <= 5) { // Condition
+            System.out.println("Number: " + i);
+            i++; // Increment
+        }
+    }
+}
+
